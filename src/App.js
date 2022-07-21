9b3854781684ee92eb318dd378c6ee6fe028827e
ryz1g <65825273+ryz1g@users.noreply.github.com>
2022-07-21 23:08:09 +0530

    Indents and code cleaning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Grid from "./components/grid";
 import SettingsPane from "./components/settingsPane";
-import {makeMove, combine} from "./utils/gameFunctions";
-
-let randomTiles = [2,4,8];
+import {initializeGame, makeMove, combine, setRandomTile} from "./utils/gameFunctions";
 
 function App() {
   const [rows, setRows] = React.useState(4);
@@ -11,40 +9,8 @@ function App() {
   const [gameState, setGameState] = React.useState([]);
 
   const initGame = () => {
-    initializeGame(setGameState, rows, columns);
-  }
-
-  const initializeGame = (setGameState, r, c) => {
-    let game = [];
-    for(let i=0;i<r;i++) {
-      let tmpRow = [];
-      for(let j=0;j<c;j++) {
-        tmpRow.push(-1);
-      }
-      game.push(tmpRow);
-    }
-    setRandomTile(game);
-    setRandomTile(game);
-    setGameState(game);
-  }
-
-  const setRandomTile = (gameState) => {
-    let emptyPlaces = [];
-    for(let i=0;i<gameState.length;i++) {
-      for(let j=0;j<gameState[0].length;j++) {
-        if(gameState[i][j]===-1) {
-          let tmp = [];
-          tmp.push(i);
-          tmp.push(j);
-          emptyPlaces.push(tmp);
-        }
-      }
-    }
-    if(emptyPlaces.length===0) {
-      initGame();
-    }
-    let rand = Math.floor(Math.random()*emptyPlaces.length);
-    gameState[emptyPlaces[rand][0]][emptyPlaces[rand][1]] = randomTiles[Math.floor(Math.random()*randomTiles.length)];
+    let board = initializeGame(rows, columns);
+    setGameState(board);
   }
 
   const handleMove = (direction) => {
@@ -52,8 +18,10 @@ function App() {
     do {
       makeMove(copiedGame,direction);
     }while(!combine(copiedGame, direction));
-    setRandomTile(copiedGame);
-    setGameState(copiedGame);
+    if(!setRandomTile(copiedGame)) 
+      initGame();
+    else
+      setGameState(copiedGame);
   }
 
   return (
--- src/utils/gameFunctions.js
+++ src/utils/gameFunctions.js
@@ -40,110 +40,142 @@ export function solveColumns(gameState, move) {
   }
   
 export function solveRows(gameState, move) {
-if(move==="left") {
-    for(let r=0;r<gameState.length;r++) {
-    let minus=0;
-    let nums=[];
-    for(let c=0;c<gameState[0].length;c++) {
-        if(gameState[r][c]===-1) minus++;
-        else {
-        nums.push(gameState[r][c]);
+    if(move==="left") {
+        for(let r=0;r<gameState.length;r++) {
+            let minus=0;
+            let nums=[];
+            for(let c=0;c<gameState[0].length;c++) {
+                if(gameState[r][c]===-1) minus++;
+                else {
+                    nums.push(gameState[r][c]);
+                }
+            }
+            let index=0;
+            nums.map((num) => {
+                gameState[r][index++]=num;
+            });
+            while(minus--!==0) {
+                gameState[r][index++]=-1;
+            }
         }
     }
-    let index=0;
-    nums.map((num) => {
-        gameState[r][index++]=num;
-    });
-    while(minus--!==0) {
-        gameState[r][index++]=-1;
-    }
-    }
-}
-else {
-    for(let r=0;r<gameState.length;r++) {
-    let minus=0;
-    let nums=[];
-    for(let c=gameState[0].length-1;c>=0;c--) {
-        if(gameState[r][c]===-1) minus++;
-        else {
-        nums.push(gameState[r][c]);
+    else {
+        for(let r=0;r<gameState.length;r++) {
+        let minus=0;
+        let nums=[];
+        for(let c=gameState[0].length-1;c>=0;c--) {
+            if(gameState[r][c]===-1) minus++;
+            else {
+                nums.push(gameState[r][c]);
+            }
+        }
+        let index=gameState[0].length-1;
+        nums.map((num) => {
+            gameState[r][index--]=num;
+        });
+        while(minus--!==0) {
+            gameState[r][index--]=-1;
+        }
         }
     }
-    let index=gameState[0].length-1;
-    nums.map((num) => {
-        gameState[r][index--]=num;
-    });
-    while(minus--!==0) {
-        gameState[r][index--]=-1;
-    }
-    }
-}
-}
-
-export  function makeMove(gameState, move) {
-if(move==="up" || move==="down") {
-    solveColumns(gameState,move);
-}
-else {
-    solveRows(gameState,move);
-}
-return gameState;
 }
 
 export function combineRows(gameState, move) {
-if(move==="left") {
-    for(let r=0;r<gameState.length;r++) {
-    for(let c=0;c<gameState[0].length-1;c++) {
-        if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r][c+1]) {
-        gameState[r][c]=2*gameState[r][c];
-        gameState[r][c+1]=-1;
+    if(move==="left") {
+        for(let r=0;r<gameState.length;r++) {
+            for(let c=0;c<gameState[0].length-1;c++) {
+                if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r][c+1]) {
+                    gameState[r][c]=2*gameState[r][c];
+                    gameState[r][c+1]=-1;
+                }
+            }
         }
     }
-    }
-}
-else {
-    for(let r=0;r<gameState.length;r++) {
-    for(let c=gameState[0].length-1;c>0;c--) {
-        if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r][c-1]) {
-        gameState[r][c]=2*gameState[r][c];
-        gameState[r][c-1]=-1;
+    else {
+        for(let r=0;r<gameState.length;r++) {
+            for(let c=gameState[0].length-1;c>0;c--) {
+                if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r][c-1]) {
+                    gameState[r][c]=2*gameState[r][c];
+                    gameState[r][c-1]=-1;
+                }
+            }
         }
-    }
-    }
-} 
+    } 
 }
 
 export function combineColumns(gameState, move) {
-console.log(move);
-if(move==="up") {
-    for(let c=0;c<gameState[0].length;c++) {
-    for(let r=0;r<gameState.length-1;r++) {
-        if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r+1][c]) {
-        gameState[r][c]=2*gameState[r][c];
-        gameState[r+1][c]=-1;
+    if(move==="up") {
+        for(let c=0;c<gameState[0].length;c++) {
+            for(let r=0;r<gameState.length-1;r++) {
+                if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r+1][c]) {
+                    gameState[r][c]=2*gameState[r][c];
+                    gameState[r+1][c]=-1;
+                }
+            }
         }
     }
-    }
-}
-else {
-    for(let c=0;c<gameState[0].length;c++) {
-    for(let r=gameState.length-1;r>0;r--) {
-        if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r-1][c]) {
-        gameState[r][c]=2*gameState[r][c];
-        gameState[r-1][c]=-1;
+    else {
+        for(let c=0;c<gameState[0].length;c++) {
+            for(let r=gameState.length-1;r>0;r--) {
+                if(gameState[r][c]!==-1 && gameState[r][c]===gameState[r-1][c]) {
+                    gameState[r][c]=2*gameState[r][c];
+                    gameState[r-1][c]=-1;
+                }
+            }
         }
-    }
-    }
-} 
+    } 
 }
 
 export function combine(gameState, move) {
-let beforeComb = JSON.parse(JSON.stringify(gameState));
-if(move==="up" || move==="down") {
-    combineColumns(gameState,move);
+    let beforeComb = JSON.parse(JSON.stringify(gameState));
+    if(move==="up" || move==="down") {
+        combineColumns(gameState,move);
+    }
+    else {
+        combineRows(gameState,move);
+    }
+    return JSON.stringify(beforeComb)===JSON.stringify(gameState);
 }
-else {
-    combineRows(gameState,move);
+
+export  function makeMove(gameState, move) {
+    if(move==="up" || move==="down") {
+        solveColumns(gameState,move);
+    }
+    else {
+        solveRows(gameState,move);
+    }
 }
-return JSON.stringify(beforeComb)===JSON.stringify(gameState);
+
+export function initializeGame(r, c) {
+    let game = [];
+    for(let i=0;i<r;i++) {
+        let tmpRow = [];
+        for(let j=0;j<c;j++) {
+            tmpRow.push(-1);
+        }
+        game.push(tmpRow);
+    }
+    setRandomTile(game);
+    setRandomTile(game);
+    return game;
+}
+
+let randomTiles = [2,4,8];
+
+export function setRandomTile(gameState) {
+    let emptyPlaces = [];
+    for(let i=0;i<gameState.length;i++) {
+        for(let j=0;j<gameState[0].length;j++) {
+            if(gameState[i][j]===-1) {
+                let tmp = [];
+                tmp.push(i);
+                tmp.push(j);
+                emptyPlaces.push(tmp);
+            }
+        }
+    }
+    if(emptyPlaces.length===0) return false;
+    let rand = Math.floor(Math.random()*emptyPlaces.length);
+    gameState[emptyPlaces[rand][0]][emptyPlaces[rand][1]] = randomTiles[Math.floor(Math.random()*randomTiles.length)];
+    return true;
 }
